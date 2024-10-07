ae14d24855de4ea33c4fa6863b60f3910583b534
Seochan Moon <moonsc1110@gmail.com>
2024-10-08 00:22:52 +0900

    refactor: Add itemManager.java

---
+++ src/engine/ItemManager.java
@@ -0,0 +1,87 @@
+package engine;
+
+import entity.Ship;
+
+import java.awt.*;
+import java.util.Random;
+
+public class ItemManager {
+    private ItemType itemType;
+    private boolean GoastActive;
+    private int shotNum;
+
+    public ItemManager() {
+        this.itemType = null;
+        this.GoastActive = false;
+        this.shotNum = 1;
+    }
+
+    private enum ItemType {
+        Bomb,
+        LineBomb,
+        Barrier,
+        Goast,
+        TimeStop,
+        MultiShot
+    }
+
+    public void dropItem() {
+        if (Math.random() < 0.99) {
+            Random rand = new Random();
+            switch (rand.nextInt(6)) {
+                case 0:
+                    this.itemType = ItemType.Bomb;
+                    break;
+                case 1:
+                    this.itemType = ItemType.LineBomb;
+                    break;
+                case 2:
+                    this.itemType = ItemType.Barrier;
+                    break;
+                case 3:
+                    this.itemType = ItemType.Goast;
+                    break;
+                case 4:
+                    this.itemType = ItemType.TimeStop;
+                    break;
+                case 5:
+                    this.itemType = ItemType.MultiShot;
+                    break;
+            }
+        } else {
+            this.itemType = null;
+        }
+    }
+
+    public void operateBomb() {}
+
+    public void operateLineBomb() {}
+
+    public void operateBarrier() {}
+
+    public void operateGoast(Ship ship) {
+        this.GoastActive = true;
+        ship.setColor(Color.DARK_GRAY);
+        new Thread(() -> {
+            try {
+                Thread.sleep(3000);  // 3초 후 고스트 모드 해제
+                this.GoastActive = false;
+                ship.setColor(Color.GREEN);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
+    public void operateTimeStop() {}
+
+    public void operateMultiShot() {
+        if (this.shotNum < 3) {
+            this.shotNum++;
+        }
+    }
+
+    public int getShotNum() {
+        return shotNum;
+    }
+}
