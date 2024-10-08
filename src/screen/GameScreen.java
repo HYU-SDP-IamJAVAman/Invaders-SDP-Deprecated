5efdda37333657ef9425e056665d8734b9764e99
Seochan Moon <moonsc1110@gmail.com>
2024-10-08 19:49:40 +0900

    style: Remove unused line

--- src/screen/GameScreen.java
+++ src/screen/GameScreen.java
@@ -1,6 +1,5 @@
 package screen;
 
-import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.util.HashSet;
 import java.util.List;
@@ -9,7 +8,6 @@ import java.util.Iterator;
 
 import engine.*;
 import entity.*;
-import engine.ItemManager.ItemType;
 
 /**
  * Implements the game screen, where the action happens.
