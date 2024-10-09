c8ff53e469401821d47dd16918bb5686d4412abf
Seochan Moon <moonsc1110@gmail.com>
2024-10-09 19:08:33 +0900

    feat: Add bomb item
    Destroy enemies in a 3x3 area that can eliminate the most enemies when the Bomb item is activated by iterating through the enemyShips.

--- src/engine/ItemManager.java
+++ src/engine/ItemManager.java
@@ -7,9 +7,11 @@ import entity.Barrier;
 
 import java.awt.*;
 import java.util.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map.Entry;
 import java.util.AbstractMap.SimpleEntry;
+import java.util.Random;
 
 /**
  * Manages item logic
@@ -21,13 +23,13 @@ public class ItemManager {
     private static final int ITEM_DROP_PROBABILITY = 99;
 
     private ItemType itemType;
-    private boolean GhostActive;
     private boolean timeStopActive;
+    private boolean ghostActive;
     private int shotNum;
     private Random rand;
-    private boolean isMaxShotNum = false;
     private Ship ship;
     private EnemyShipFormation enemyShipFormation;
+    private boolean isMaxShotNum;
 
     public ItemManager(Ship ship, EnemyShipFormation enemyShipFormation) {
         this.itemType = null;
@@ -36,6 +38,7 @@ public class ItemManager {
         this.rand = new Random();
         this.ship = ship;
         this.enemyShipFormation = enemyShipFormation;
+        this.isMaxShotNum = false;
     }
 
     public enum ItemType {
@@ -52,6 +55,7 @@ public class ItemManager {
     }
 
     public ItemType selectItemType() {
+
         if (!isMaxShotNum) {
             switch (rand.nextInt(6)) {
                 case 0:
@@ -96,13 +100,73 @@ public class ItemManager {
         return this.itemType;
     }
 
-    public void operateBomb() {}
+    public Entry<Integer, Integer> operateBomb() {
+        int addScore = 0;
+        int addShipsDestroyed = 0;
+
+        List<List<EnemyShip>> enemyships = this.enemyShipFormation.getEnemyShips();
+        int enemyShipsSize = enemyships.size();
+
+        int maxCnt = -1;
+        int maxRow = 0, maxCol = 0;
+
+        for (int i = 0; i <= enemyShipsSize - 3; i++) {
+
+            List<EnemyShip> rowShips = enemyships.get(i);
+            int rowSize = rowShips.size();
+
+            for (int j = 0; j <= rowSize - 3; j++) {
+
+                int currentCnt = 0;
+
+                for (int x = i; x < i + 3; x++) {
+
+                    List<EnemyShip> subRowShips = enemyships.get(x);
+
+                    for (int y = j; y < j + 3; y++) {
+                        EnemyShip ship = subRowShips.get(y);
+
+                        if (ship != null && !ship.isDestroyed())
+                            currentCnt++;
+                    }
+                }
+
+                if (currentCnt > maxCnt) {
+                    maxCnt = currentCnt;
+                    maxRow = i;
+                    maxCol = j;
+                }
+            }
+        }
+
+        List<EnemyShip> targetEnemyShips = new ArrayList<>();
+        for (int i = maxRow; i < maxRow + 3; i++) {
+            List<EnemyShip> subRowShips = enemyships.get(i);
+            for (int j = maxCol; j < maxCol + 3; j++) {
+                EnemyShip ship = subRowShips.get(j);
+
+                if (ship != null && !ship.isDestroyed())
+                    targetEnemyShips.add(ship);
+            }
+        }
+
+        if (!targetEnemyShips.isEmpty()) {
+            for (EnemyShip destroyedShip : targetEnemyShips) {
+                addScore += destroyedShip.getPointValue();
+                addShipsDestroyed++;
+                enemyShipFormation.destroy(destroyedShip);
+            }
+        }
+
+        return new SimpleEntry<>(addScore, addShipsDestroyed);
+    }
 
     public Entry<Integer, Integer> operateLineBomb() {
         int addScore = 0;
         int addShipsDestroyed = 0;
 
         List<List<EnemyShip>> enemyships = this.enemyShipFormation.getEnemyShips();
+
         int targetRow = -1;
         int maxCnt = -1;
 
